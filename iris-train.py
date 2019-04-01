2477f4f7eb554c14526655014ed327dd42e5843d
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-01 11:13:31 +0900

    irisTest

--- iris-train.py
+++ iris-train.py
@@ -26,41 +26,48 @@ with open('iris.csv','r',encoding='utf-8') as fp:
         # print(cols) 데이터를 한줄 씩 읽어옴,
 
         csv.append(cols)
-# print(csv)  데이터를 2차원 배열에 넣어줌.
-        # [['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Name'], [5.1, 3.5, 1.4, 0.2, 'Iris-setosa'],
+     # print(csv)  데이터를 2차원 배열에 넣어줌.
+     # [['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Name'], [5.1, 3.5, 1.4, 0.2, 'Iris-setosa'],
 
-# 가장 앞 줄의 헤더 제거
-del csv[0]
-# print(csv) : [[5.1, 3.5, 1.4, 0.2, 'Iris-setosa'], [4.9, 3.0, 1.4, 0.2, 'Iris-setosa'],
+    # 가장 앞 줄의 헤더 제거
+    del csv[0]
+    # print(csv) : [[5.1, 3.5, 1.4, 0.2, 'Iris-setosa'], [4.9, 3.0, 1.4, 0.2, 'Iris-setosa'],
 
-# 데이터 셔플하기(섞기) - 붓꽃의 csv 데이터가 중복되어있으닌까 잘 섞어야 한다.
-random.shuffle(csv)
-# print(csv)
+    # 데이터 셔플하기(섞기) - 붓꽃의 csv 데이터가 중복되어있으닌까 잘 섞어야 한다.
+    random.shuffle(csv)
+    # print(csv)
 
-# 학습전용 데이터와 텍스트 전용 데이터 분할하기(2:1 비율)
-total_len = len(csv)
-train_len = int(total_len * 2/3)
-train_data = []
-train_label = []
-test_data = []
-test_label = []
+    # 학습전용 데이터와 텍스트 전용 데이터 분할하기(2:1 비율)
+    total_len = len(csv)
+    train_len = int(total_len * 2/3)
+    # print(total_len)  150
+    # print(train_len)  100
+    train_data = []
+    train_label = []
+    test_data = []
+    test_label = []
 
-for i in range(total_len):
-    data = csv[i][:4]
-    label = csv[i][4]
-    if i < train_len:
-        train_data.append(data)
-        train_label.append(label)
-    else:
-        test_data.append(data)
-        test_label.append(label)
+    for i in range(total_len):
+        data = csv[i][:4]
+        label = csv[i][4]
+        if i < train_len:
+            train_data.append(data)
+            train_label.append(label)
+        else:
+            test_data.append(data)
+            test_label.append(label)
 
-# 데이터를 학습하고 예측하기
-clf = svm.SVC()
-clf.fit(train_data, train_label)
-pre = clf.predict(test_data)
-# print(test_data)
+    # print(train_data) #훈련시킨 문제 : [[5.7, 3.8, 1.7, 0.3], [4.4, 2.9, 1.4, 0.2],
+    # print(train_label) # 훈련시킨 답 : ['Iris-setosa', 'Iris-setosa',
 
-# 정답구하기
-ac_score = metrics.accuracy_score(test_label,pre)
-print("정답률 : ",ac_score)
+
+    # 데이터를 학습하고 예측하기
+    clf = svm.SVC()
+    clf.fit(train_data, train_label)
+    pre = clf.predict(test_data)
+    # print(pre) # 예측
+    # print(test_label) # 진짜 답
+
+    # 정답구하기
+    ac_score = metrics.accuracy_score(test_label,pre)
+    print("정답률 : ",ac_score)  # 정답률 :  0.98
