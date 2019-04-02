06771c3ff6a351ffe170a0a3620e2fa8a62eb4e7
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-02 18:21:38 +0900

    matplotlib

---
+++ day0402/matplotlibTest.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = [10,20,30,40,50]
+
+plt.plot(data)
+plt.show()
+
+x = np.array([1,2,3,4,5])
+y = x ** 2
+
+plt.plot(x,y,'ro')
+plt.show()
+
+
+
+
+
