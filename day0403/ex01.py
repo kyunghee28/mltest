5b0ff9c80e06df377f3a3dcb85b404f4dae26ff8
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-03 09:37:56 +0900

    numpyTest

---
+++ day0403/ex01.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#np.zeros   배열의 요소를 0으로 채워주세요
+#np.ones    배열의 요소를 1으로 채워주세요
+#np.full    배열의 요소를 특정값을 채워주세요
+
+a = np.zeros(10, dtype='int32')
+b = np.ones(5, dtype=np.int32)
+c = np.full(7,100)
+
+print(a)
+print(b)
+print(c)
+
