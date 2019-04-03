f38ca6e0ba84cb4386dfd9af1cee3ea03e5f61b3
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-03 10:23:23 +0900

    numpyTest

--- day0403/ex01.py
+++ day0403/ex01.py
@@ -14,6 +14,7 @@ print(b)
 print(c)
 
 print('*'*20)
+
 # 예제 2. 2 차원 배열을 특정값으로 채워지도록.
 a = np.zeros([2,3], dtype=np.int32)
 b = np.ones([5,5], dtype=np.int32)
@@ -24,6 +25,7 @@ print(b)
 print(c)
 
 print('*'*20)
+
 # 예제 3. 연속된 데이터를 갖는 numpy 배열 생성 : np.arange
 a = np.arange(10)
 print(a)
@@ -38,6 +40,7 @@ d = np.arange(5,-5,-1)
 print(d)
 
 print('*'*20)
+
 # 예제 4. 파이썬 배열을 numpy배열로 : np.array
 a = [1,2,3,4,5]
 b = np.array(a)
@@ -48,6 +51,7 @@ print(a[2]) # 결과 : 3
 print(b[2]) # 결과 : 3
 
 print('*'*20)
+
 # 예제 5. numpy배열을 파이썬 배열로 : list
 c = np.arange(5,-5,-1)
 d = list(c)
@@ -57,6 +61,7 @@ print(type(c)) # 결과 : <class 'numpy.ndarray'>
 print(type(d)) # 결과 : <class 'list'>
 
 print('*'*20)
+
 # 예제 6. 배열의 차수 , 모양(몇행 몇열인지) 확인,  요소의 자료형 확인
 e = np.array([1,2,3,4,5]) # 정수형의 numpy 배열
 f = np.array([10.5,2.7,3.5]) # 실수형의 numpy 배열
@@ -83,14 +88,16 @@ print(f.shape)   # 결과 : (3,)
 print(g.shape)   # 결과 : (2, 3) -> 2행 3열이다.
 
 print('*'*20)
-# numpy 배열의 차원을 변경 : reshape
+
+# 예제 7. numpy 배열의 차원을 변경 : reshape
 a = np.array([1,2,3,4,5,6])
 b = a.reshape([2,3]) # `1차원 배열을 2행 3열으로
 print(a)
 print(b)
 
 print('*'*20)
-# 예제 7. 차원을 변경할 때 행이나 열중에 하나를 생략
+
+# 예제 8. 차원을 변경할 때 행이나 열중에 하나를 생략
 a = np.array([1,2,3,4,5,6])
 b = a.reshape([2,-1]) # 2행으로 해주고 열은 마음데로 해줘.
 c = a.reshape([-1,3]) # 3열로 해주고 행은 마음데로 해줘.
@@ -99,22 +106,34 @@ print(b)
 print(c)
 
 print('*'*20)
-# 예제 8. 2차원 배열을 다시 1차원 배열으로
-# => 내가 직접 데이터의 수를 파악하기 보다는 shape를 통해 행,열 만큼의 배열을 생성하도록 합니다.
+# 예제 9. 2차원 배열을 다시 1차원 배열으로
+# => 내가 직접 데이터의 수를 파악하기 보다는 shape를 통해 행,열 만큼의 배열을 생성
 a = np.array([[1,2,3],[4,5,6]])
 
-# 방법1,
+# 방법1.
 print(len(a[0])) # 결과 : 3
 print(len(a))   # 결과 : 2
 b = a.reshape(len(a) * len(a[0]))
 print(b)
 
-# 방법2,
+# 방법2. shape
 row,col = a.shape
-print(row)
-print(col)
+print(row)   # 결과 : 2
+print(col)   # 결과 : 3
 c = a.reshape(row * col)
 print(c)
 
+# 방법3. size -> 배열의 요소의 수를 알 수 있다.
+print(a.size) # 결과 : 6
+d = a.reshape(a.size)
+print(d)
+
+# 방법4. 1차원으로 알아서 바꿔달라고 -1을 사용
+e = a.reshape(-1)
+print(e)
+
 print('*'*20)
+
+
+
 
