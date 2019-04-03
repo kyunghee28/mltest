3a132170baf8a9a9056c8f6078b8f0cc7e8e7a30
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-03 09:48:40 +0900

    numpyTest

--- day0403/ex01.py
+++ day0403/ex01.py
@@ -12,3 +12,26 @@ print(a)
 print(b)
 print(c)
 
+print('*'*20)
+# 2 차원 배열을 특정값으로 채워지도록.
+a = np.zeros([2,3], dtype=np.int32)
+b = np.ones([5,5], dtype=np.int32)
+c = np.full([5,5], 100)
+
+print(a)
+print(b)
+print(c)
+
+print('*'*20)
+# 연속된 데이터를 갖는 numpy 배열 생성 : np.arange
+a = np.arange(10)
+print(a)
+
+b = np.arange(1,11)
+print(b)
+
+c = np.arange(1,11,2)
+print(c)
+
+d = np.arange(10,-10,-1)
+print(d)
