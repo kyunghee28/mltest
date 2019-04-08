53dcce6e0fe66f68d993b1a608ac432a50cac06e
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-08 18:08:27 +0900

    mergeTest

--- day0408/ex06.py
+++ day0408/ex06.py
@@ -26,7 +26,7 @@ Index(['Last Action Hero (1993)', 'Postino, Il (The Postman) (1994)',
 
 a = df.pivot_table(values='rating', index='title', columns='gender', aggfunc='mean')
 b = a.loc[cnt_500.index]
-print(b)
+# print(b)
 '''
 gender                                                     F         M
 title                                                                 
@@ -45,3 +45,71 @@ American Beauty (1999)                              4.238901  4.347301
 
 [618 rows x 2 columns]
 '''
+
+# 예제 8. 성별별로 평점 평균의 차이가 가장 많이 나는 영화 5개를 출력해 봅시다.
+b['diff'] = (b['F']-b['M']).abs()
+# print(b)
+'''
+    b['diff'] = b['F']-b['M'] 는 음수가 나오닌까 절대값으로!! 
+    gender                                                 F         M      diff
+    title                                                                       
+    $1,000,000 Duck (1971)                          3.375000  2.761905  0.613095
+    'Night Mother (1986)                            3.388889  3.352941  0.035948
+    'Til There Was You (1997)                       2.675676  2.733333 -0.057658
+
+
+    ==> 음수가 나오닌까 절대값을 구해야 한다. : abs( ) 함수 사용
+        gender                                                     F  ...      diff
+    title                                                         ...          
+    Last Action Hero (1993)                             2.662338  ...  0.182432
+    Postino, Il (The Postman) (1994)                    4.125628  ...  0.052780
+    Emma (1996)                                         4.061151  ...  0.406443
+    Guns of Navarone, The (1961)                        4.061224  ...  0.107582
+'''
+
+c = b.sort_values(by='diff', ascending=False)   # ascending=False : 내림차순
+# print(c.head(5))
+'''
+gender                                         F         M      diff
+title                                                               
+Dirty Dancing (1987)                    3.790378  2.959596  0.830782
+Good, The Bad and The Ugly, The (1966)  3.494949  4.221300  0.726351
+Dumb & Dumber (1994)                    2.697987  3.336595  0.638608
+Evil Dead II (Dead By Dawn) (1987)      3.297297  3.909283  0.611985
+Grease (1978)                           3.975265  3.367041  0.608224
+'''
+
+
+# 예제 9. 남자가 더 좋아하는 영화 5개
+a = df.pivot_table(values='rating', index='title', columns='gender', aggfunc='mean')
+b = a.loc[cnt_500.index]
+b['M-F'] = b['M']-b['F']
+m = b.sort_values(by='M-F', ascending=False)   # ascending=False : 내림차순
+print(m.head(5))
+'''
+gender                                         F         M       M-F
+title                                                               
+Good, The Bad and The Ugly, The (1966)  3.494949  4.221300  0.726351
+Dumb & Dumber (1994)                    2.697987  3.336595  0.638608
+Evil Dead II (Dead By Dawn) (1987)      3.297297  3.909283  0.611985
+Caddyshack (1980)                       3.396135  3.969737  0.573602
+Animal House (1978)                     3.628906  4.167192  0.538286
+'''
+
+# 예제 10. 여자가 더 좋아하는 영화 5개
+a = df.pivot_table(values='rating', index='title', columns='gender', aggfunc='mean')
+b = a.loc[cnt_500.index]
+b['F-M'] =b['F']-b['M']
+f = b.sort_values(by='F-M', ascending=False)   # ascending=False : 내림차순
+print(f.head(5))
+'''
+gender                                        F         M       F-M
+title                                                              
+Dirty Dancing (1987)                   3.790378  2.959596  0.830782
+Grease (1978)                          3.975265  3.367041  0.608224
+Rocky Horror Picture Show, The (1975)  3.673016  3.160131  0.512885
+Mary Poppins (1964)                    4.197740  3.730594  0.467147
+Sound of Music, The (1965)             4.233677  3.783418  0.450259
+'''
+
+
