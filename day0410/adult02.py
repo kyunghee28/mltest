16c00a65d2ab634810faa851a00a34fcf4713f14
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-10 15:18:26 +0900

    pandas

--- day0410/adult02.py
+++ day0410/adult02.py
@@ -10,3 +10,21 @@ data = pd.read_csv('../data/adult.data.txt' ,header=None, names=names)
 data = data[['age','workclass','education','sex','hours-per-week','occupation','income']]
 
 workkind = data['workclass'].unique()
+print(workkind)
+print(len(workkind))
+
+jobkind = data['occupation'].unique()
+print(jobkind)
+print(len(jobkind))
+
+educationkind = data['education'].unique()
+print(educationkind)
+print(len(educationkind))
+
+sexkind = data['sex'].unique()
+print(sexkind)
+print(len(sexkind))
+
+incomekind = data['income'].unique()
+print(incomekind)
+print(len(incomekind))
