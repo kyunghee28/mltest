ca0d45c6095ce4f5732c0b4eaa91c624c341aa20
kyunghee28 <pori4339@naver.com>
2019-04-10 17:29:27 +0900

    LabelBinarizer

---
+++ day0410/sklerarn01.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn import preprocessing
+
+age = [28,20,38,34]
+
+b_age = preprocessing.LabelBinarizer().fit_transform(age)
+print(b_age)
+'''
+[[0 1 0 0]
+ [1 0 0 0]
+ [0 0 0 1]
+ [0 0 1 0]]
+'''
+
+addr = ['서울','마산','서울','대전','광주']
+b_addr = preprocessing.LabelBinarizer().fit_transform(addr)
+print(b_addr)
+
+'''     
+[[0 0 0 1]  서울
+ [0 0 1 0]  마산
+ [0 0 0 1]  서울
+ [0 1 0 0]  대전
+ [1 0 0 0]] 광주
+'''
+
+# 데이터의 value 만큼 속성이 만들어 진다.
+
