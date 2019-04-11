0f5882bd313b7339c5fdb5bd260c6655b8d9bc66
kyunghee28 <pori4339@naver.com>
2019-04-11 15:43:07 +0900

    LogisticRegression

--- day0411/ex02.py
+++ day0411/ex02.py
@@ -1,84 +1,146 @@
 import numpy as np
 import pandas as pd
+from sklearn import linear_model, model_selection
 
 names = ['age','workclass','fnlwgt','education','education-num','marital-status',
          'occupation','relationship','race','sex','capital-gain','capital-loss',
          'hours-per-week','native-country','income']
 
-info = pd.read_csv('../data/adult.data.txt' ,header=None, names=names)
+df = pd.read_csv('../data/adult.data.txt' ,header=None, names=names)
 
-data = info[['age','workclass','education','sex','hours-per-week','occupation','income']]
+df_choice = df[['age','workclass','education','sex','race','hours-per-week','occupation','income']]
 
-# 위에 데아터프레임을 one-hot을 하면 몇개의 feature가 생성될지 예측
-# workclass 의 값의 종류의 수
-print(len(data['workclass'].unique()))    # 9
-# education 의 값의 종류의 수
-print(len(data['education'].unique()))    # 16
-# occupation 의 값의 종류의 수
-print(len(data['occupation'].unique()))    # 15
-# sex 의 값의 종류의 수
-print(len(data['sex'].unique()))    # 2
-# income 의 값의 종류의 수
-print(len(data['income'].unique()))    # 2
-
-print(9+16+15+2+2+2)  # 46  => 숫자(age,hours-per-week)는 바로 출력
-
-new_df = pd.get_dummies(data)
-
-# fancy indexing
-# 2차원 배열인 경우 원하는 데이터를 추출하기 위하여 행열을 분리하여 범위를 지정하여 slicing할 수 있다.
-# 데이터프레임[행,열]
+new_df = pd.get_dummies(df_choice)
 
 x = new_df.iloc[:,:-2]
-print(x.columns)
-
 y = new_df.iloc[:,-1]
-print(y.head())
-'''
-0    0
-1    0
-2    0
-3    0
-4    0
-Name: income_ >50K, dtype: uint8
-'''
 
 #  문제와 답의 차수 확인
 print(x.shape)  # (32561, 44)   2차원
 print(y.shape)  # (32561,)  1차원
 
-#  기계학습을 시키기 위하여
-#  Regression => 회귀 분석
-#  sklearn 의 linear_model 의 LogisticRegression()
-
-from sklearn import linear_model, model_selection
-
 # 문제 x 와 답 y를 훈련에 참여시킬 데이터와 검증을 위한 데이터로 분리
 train_x, test_x, train_y, test_y = model_selection.train_test_split(x,y)
-print(len(train_x)) # 24420
-print(len(train_y)) # 24420
-print(len(test_x))  # 8141
-print(len(test_y))  # 8141
 
 lr = linear_model.LogisticRegression()
 lr.fit(train_x,train_y) # 훈련용 데이터와 답을 갖고 학습 시키기
-r = lr.predict(test_x) # 검증용 문제를 갖고 훈련이 잘 되었는지 검증 (예측하기 - 문제로)
+r = lr.predict(test_x)  # 검증용 문제를 갖고 훈련이 잘 되었는지 검증 (예측하기 - 문제로)
 
-# 예측한 것(r)과 진짜 답하고 비교
-print(len(r))   # 8141
-print(len(test_y))  # 8141
+# 연습 ) 실 데이터를 가지고 적용시켜 봅시다.
+# 47, Private, 287828, Bachelors, 13, Married-civ-spouse, Exec-managerial, Wife, White, Female, 0, 0, 40, United-States, >50K
 
-result = r == test_y    # 예측한 결과 r과 검증을 위한 진짜 답 test_y 를 서로 비교합니다.
-print(result)
+# 알고자하는 데이터를 훈련시킨 feature 의 수와 동일하게 하기 위하여
+# 원래 원본데이터의 맨 마지막에 추가시키고 one-hot 인코딩을 합시다. (data => 원본데이터)
 
-a = result.values     # 검증한 결과가 Series 라서 value 만 뽑아온다,
-b = a[a == True]     # True 인것만 추출
-print(len(b))         # 6668
+n = [[47,' Private',' Bachelors',' Female',' White',40,' Exec-managerial',' >50K']]
+n_df = pd.DataFrame(n, columns=['age','workclass','education','sex','race','hours-per-week','occupation','income'])
 
-print('정답률 : ',len(b)/len(test_y)*100)  # 정답률 :  81.90639970519592
+print(len(df_choice))    # 32561
 
-# 정답률을 알기 위해 LogisticRegression 의 score 함수를 이용해 봅시다,
-print("정답률 : ", lr.score(test_x,test_y))
+df2 = df_choice.append(n_df, ignore_index=True)    # => 행을 추가
+print(len(df2))    # 32562
+
+print(df2.iloc[-1,:])
+'''
+age                             47
+workclass                  Private
+education                Bachelors
+sex                         Female
+race                         White
+hours-per-week                  40
+occupation         Exec-managerial
+income                        >50K
+Name: 32561, dtype: object
+'''
+
+one_hot = pd.get_dummies(df2)
+
+print(len(one_hot.columns)) # 51
+print(len(new_df.columns))  # 51
+
+pred_x = np.array(one_hot.iloc[-1,:-2]).reshape(1,-1)
+pred_y = lr.predict(pred_x)
+
+print(pred_y)   # [0]
+'''
+1 이 나오면 5만달라 이상 버는 것
+0 이 나오면 5만달라 미만 버는 것
+'''
+
+# 연습 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
