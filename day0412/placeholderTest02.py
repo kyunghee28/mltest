7ab84ca92eda7c2b069bc558352c8bc92c2e7136
kyunghee28 <pori4339@naver.com>
2019-04-12 13:04:46 +0900

    placeholder

--- day0412/placeholderTest02.py
+++ day0412/placeholderTest02.py
@@ -1,42 +1,32 @@
-'''
-placeholder 란,
-    마치 데이터베이스의 PrepareStatment 처럼 질의문에 ? 를 생각하면 된다.
-    사용자가 입력한 값을 질의문에 대응시키기 위하여 ? 를 대신하듯이
-    어떤 수식에 대응시키기 위한 변수의 틀을 미리 만들어 두는 개념.
-    
-    예를 들어, 다음의 수식을 보면 
-        a = [1,2,3]
-        b = a * 2
-        b 는 정해진 배열 [1,2,3] * 2 만 할 줄 안다.
-        
-    만약, 어떤요소와 배열이라도 연산시키고자 한다면 위와 같이 값을 구체화 하지 않고 
-        3개 짜리 배열이라고 틀을 만들어 두면 어떤 요소의 배열이라도 연산 시킬 수 있다.
-'''
-
 import tensorflow as tf
 
-# 어떤 값이라도 담을 수 있는 정수형 3개를 담을 수 있는 placeholder 정의
-# placeholder 이름은 a 라고 지정
-a = tf.placeholder(tf.int32, [3])   # 32비트 정수 자료형 3개를 가진 배열
+# 크가가 상관없응 정수형 1차원 배열의 placeholder 를 만들고
+# 				이것의 *2 한 결과를 tensor를 이용하여 출력해 봅시다.
 
-# 위에서 만든 placeholder 가 사용되는 수식(데이터 플로우 그래프) 만들기
-b =tf.constant(2)   # 상수 정의
-x_op = a * b       # 배열의 모든 값을 2배한는 연산 정의
+# 크기가 정해지지 않는 1차원 배열에 placeholder 만들기
+#     ex. 2차원 배열일 경우 [None][None]
+arr = tf.placeholder(tf.int32, [None])
 
-# 위의 수식 실행
-# tensorflow 의 Session 을 얻어옴
+# arr의 *2 한 수식 만들기
+
+# 2를 직접 표현하지 않고 tensorflow 의 상수로!!
+b =tf.constant(2)
+
+# 위의 상수를 이용하여 수식 만들기
+x_op = arr * b
+
+# Session 을 얻어와 위의 수식을 실행
+# 실행 시킬 땐 placeholder 의 값을 지정해야 한다1!
 sess = tf.Session()
 
-# 위의 수식 x_op 실행
-# x_op 엔 결정되지 않은 값 (placeholder) 를 갖고 있기 때문에
-# placeholder 의 값을 설정해 줘야 한다.  => feed_dict={ key-placeholder 이름: value }
-r1 = sess.run(x_op, feed_dict={ a:[1,2,3] })   # a 에 [1,2,3] 을 적용 시켜주세요
-print(r1)   # 결과 : [2 4 6]
+r1 = sess.run(x_op, feed_dict={ arr:[1,2,3,4,5,6] })    # 딕셔너리 상태로 적용
+print(r1)   # 결과 :  [ 2  4  6  8 10 12]
 
-r2 = sess.run(x_op, feed_dict={ a:[10,20,30] })
-print(r2)    # 결과 : [20 40 60]
+r2 = sess.run(x_op, feed_dict={ arr:[10,20] })
+print(r2)   # 결과 : [20 40]
 
-row = [4,5,6]
-r3 = sess.run(x_op, feed_dict={ a:row })
-print(r3)    # 결과 : [ 8 10 12]
+row = [4,5,-2,9]
+r3 = sess.run(x_op, feed_dict={ arr:row })
+print(r3)   # 결과 : [ 8 10 -4 18]
+
 
