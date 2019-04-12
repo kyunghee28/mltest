cbdfd9f70610517e1ee5c9068033dccb2a195d53
kyunghee28 <pori4339@naver.com>
2019-04-12 11:37:32 +0900

    tensorflow

---
+++ day0412/MarkdownTest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = 5
+a
+
+
+# # Markdown으로 메모 작성하기
+#     
+#     - cell > sell type > Markdown 을 누르면 된다. 
+#     
+#     - 마크다운으로 메모를 작성할 수 있다.
+#     
+# # 목표
+#     
+#     - jupyter Notebook
+#     - 시각적으로 기계학습 살펴보기
+
+# In[4]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-20,20,0.1)
+y = np.sin(x)
+plt.plot(x,y)
+
+
+
+
+
